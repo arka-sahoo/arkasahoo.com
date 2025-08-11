78ec147cced57f5b0cf893bc35c775298b1e7a48
Arka Sahoo <arkasahoo2005@gmail.com>
2025-08-10 21:55:39 -0500

    Made some changes to app file

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { Home } from "./pages/Home.jsx";
 import { About } from "./pages/About.jsx";
 import { Projects } from "./pages/Projects.jsx";
 import { Contact } from "./pages/Contact.jsx";
-import { Courses } from "./pages/Courses.jsx";
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/courses" element={<Courses />} />
       </Routes>
     </>
   );
