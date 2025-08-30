aaf1f4eeee45700f795d17c2df7d19d6f3d7840e
Arka Sahoo <arkasahoo2005@gmail.com>
2025-08-30 11:03:14 -0500

    Update text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ export const Home = () => {
   return (
     <div className="bg-pink-200 min-h-screen flex items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-600">
-        Welcome to my personal website
+        Welcome to my personal website!
       </h1>
     </div>
   );
