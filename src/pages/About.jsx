499cfa28986d7130528497d601d9b1f1830cf922
Arka Sahoo <arkasahoo2005@gmail.com>
2025-08-30 11:50:27 -0500

    changed the background color

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 export const About = () => {
   return (
-    <div className="min-h-screen w-full p-8 bg-lime-300">
+    <div className="min-h-screen w-full p-8 bg-sky-300">
       <h1 className="text-3xl font-bold text-blue-700">About me</h1>
       <p className="ml-1 text-gray-700">
         I am Arka Sahoo, a student at The University of Nebraska at Omaha. My major is Computer Science with a concentration in Software Enineering and my minor is Management Information Systems.
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 export const Contact = () => {
   return (
-    <div className="bg-lime-300 min-h-screen flex items-center justify-center">
+    <div className="bg-sky-300 min-h-screen flex items-center justify-center">
       <div className="bg-white rounded-lg shadow p-8 max-w-md w-full">
         <h1 className="text-4xl font-bold text-blue-600 mb-4">Contact</h1>
         <p className="ml-1 text-gray-700">
