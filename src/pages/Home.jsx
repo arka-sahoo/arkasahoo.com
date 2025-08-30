140eacaa4bff2f7b3a23460ff48abb6b77a07d24
Arka Sahoo <arkasahoo2005@gmail.com>
2025-08-30 11:46:51 -0500

    changed the background color

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export const Home = () => {
   return (
-    <div className="bg-green-300 min-h-screen flex items-center justify-center">
+    <div className="bg-lime-300 min-h-screen flex items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-600">
         Welcome to my personal website!
       </h1>
